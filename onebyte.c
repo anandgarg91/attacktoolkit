28d2a350010672bf015285b1d91c745393573301
anandgarg91 <anandgarg91@gmail.com>
2019-04-09 14:44:08 +0800

    passing the output to file and converting the pcap fromat through text2cap

--- onebyte.c
+++ onebyte.c
@@ -1,11 +1,13 @@
 #include "stdio.h"
 #include<time.h>
 #include<pcap.h>
+#include<stdlib.h>
 
 void print_packet_info(const u_char *packet, struct pcap_pkthdr packet_header);
 
 int main(void)
 {
+
 const u_char *packet;
 struct pcap_pkthdr packet_header;
 pcap_t *pcap_open_offline(const char *fname, char *errbuf);
@@ -26,8 +28,8 @@ return 0;
 
 void print_packet_info(const u_char *packet, struct pcap_pkthdr packet_header){
 	
-	printf("pcaket cap length: %d\n", packet_header.caplen);
-	printf("pcaket total len %d\n", packet_header.len);
+	printf("packet cap length: %d\n", packet_header.caplen);
+	printf("packet total len %d\n", packet_header.len);
 	unsigned char store[100];
 	for(int i=0;i<packet_header.len;i++)
 		{
@@ -47,10 +49,13 @@ void print_packet_info(const u_char *packet, struct pcap_pkthdr packet_header){
 			
 		}
 printf("\n");
+FILE *fptr;
+fptr=fopen("testing.pcap","w");
 for(int j=0;j<packet_header.len;j++)
 {
-printf("%x",store[j]);
+fprintf(fptr,"%x",store[j]);
 }
+fclose(fptr);
 }
 
 
