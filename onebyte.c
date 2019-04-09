3dd41939440c692faebe585dddb795a5ff7fda0d
anandgarg91 <anandgarg91@gmail.com>
2019-04-09 13:24:20 +0800

    changing the ttl and storing

--- onebyte.c
+++ onebyte.c
@@ -25,12 +25,32 @@ return 0;
 }
 
 void print_packet_info(const u_char *packet, struct pcap_pkthdr packet_header){
+	
 	printf("pcaket cap length: %d\n", packet_header.caplen);
 	printf("pcaket total len %d\n", packet_header.len);
-	const u_char *temp_pointer = packet+22;
-	printf("%x", *temp_pointer);
-		
+	unsigned char store[100];
+	for(int i=0;i<packet_header.len;i++)
+		{
+			if(i==22)	
+				{
+					const u_char *temp = (packet+22);
+					store[i]=0x33;
+					printf("%x", 0x33);
+				}
+			else	
+				{
+					const u_char *temp = packet+i;
+					store[i]=*temp;
+					printf("%x", *temp);
+				}
+				
+			
+		}
+printf("\n");
+for(int j=0;j<packet_header.len;j++)
+{
+printf("%x",store[j]);
 }
-
+}
 
 
